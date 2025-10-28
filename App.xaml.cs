ccd3996cfb958a21c413451e961c027c3b2d95d3
j0s3s4nch3z <j0s3.s4nch3z.1992@gmail.com>
2025-10-27 22:13:19 -0300

    first commit

---
+++ App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Gestion;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
