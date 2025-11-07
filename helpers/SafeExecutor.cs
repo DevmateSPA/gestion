9016fb85099db387d091309823359ba55241fe58
Roony11-1 <eltheasasin@gmail.com>
2025-11-07 00:34:04 -0300

    Agregado loading para async(?

--- helpers/SafeExecutor.cs
+++ helpers/SafeExecutor.cs
@@ -8,11 +8,16 @@ public static class SafeExecutor
     {
         try
         {
+            dialogService.ShowLoading();
             await action();
         }
         catch (Exception ex)
         {
             dialogService.ShowError($"{mensajeError}: {ex.Message}");
+        }
+        finally
+        {
+            dialogService.HideLoading();
         }
     }
 }
