910ee186a184e5da202fff3f9ba7ec6b03562548
Roony11-1 <eltheasasin@gmail.com>
2025-11-13 21:36:52 -0300

    Quitada implementaciones

--- core/services/FacturaService.cs
+++ core/services/FacturaService.cs
@@ -6,15 +6,6 @@ namespace Gestion.core.services;
 
 public class FacturaService : BaseService<Factura>, IFacturaService
 {
-    private readonly IFacturaRepository _facturaRepository;
     public FacturaService(IFacturaRepository facturaRepository)
-        : base(facturaRepository)
-    {
-        _facturaRepository = facturaRepository;
-    }
-        
-    public async Task<List<Factura>> FindAllConDetalle()
-    {
-        return await _facturaRepository.FindAllConDetalles();
-    }
+        : base(facturaRepository) {}
 }
