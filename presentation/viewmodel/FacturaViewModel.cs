4462aa798185c3c69688fd6e93c4a9756cab1b21
Roony11-1 <eltheasasin@gmail.com>
2025-11-13 21:37:32 -0300

    Quitado metodos que le corresponden al detalleviewModel

--- presentation/viewmodel/FacturaViewModel.cs
+++ presentation/viewmodel/FacturaViewModel.cs
@@ -7,51 +7,7 @@ namespace Gestion.presentation.viewmodel;
 
 public class FacturaViewModel : EntidadViewModel<Factura>
 {
-    private readonly IDetalleService _detalleService;
     public ObservableCollection<Factura> Facturas => Entidades;
-    public ObservableCollection<Detalle> Detalles = new ObservableCollection<Detalle>();
-    public FacturaViewModel(IFacturaService facturaService, IDetalleService detalleService, IDialogService dialogService)
-        : base(facturaService, dialogService)
-    {
-        _detalleService = detalleService;
-    }
-
-    public async Task saveDetails(List<Detalle> detalles)
-    {
-        foreach (var detalle in detalles)
-        {
-            await SafeExecutor.RunAsync(async () =>
-            {
-                await _detalleService.Save(detalle);
-
-            }, _dialogService, $"Error al guardar el producto {detalle.Producto}");
-        }
-    }
-
-    public async Task updateDetails(List<Detalle> detalles)
-    {
-        foreach (var detalle in detalles)
-        {
-            await SafeExecutor.RunAsync(async () =>
-            {
-                await _detalleService.Update(detalle);
-
-            }, _dialogService, $"Error al actualizar el producto {detalle.Producto}");
-        }
-
-        
-    }
-        
-    public async Task LoadAllDetalles()
-    {
-        await SafeExecutor.RunAsync(async () =>
-        {
-            var detalles = await _detalleService.FindAll();
-
-            Detalles.Clear();
-            foreach (var d in detalles)
-                Detalles.Add(d);
-
-        }, _dialogService, "Error al cargar los detalles de facturas");
-    }
+    public FacturaViewModel(IFacturaService facturaService, IDialogService dialogService)
+        : base(facturaService, dialogService) {}
 }
