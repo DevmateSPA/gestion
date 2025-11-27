eaa49268fa6571ff071317fc50d7d0c692bcecf5
Roony11-1 <eltheasasin@gmail.com>
2025-11-27 14:19:06 -0300

    cliente goldo

--- core/model/Cliente.cs
+++ core/model/Cliente.cs
@@ -7,56 +7,27 @@ public class Cliente : IModel
 {
     public long Id { get; set; }
     [Required]
-    public string Rut { get; private set; } = string.Empty;
+    public string Rut { get; set; } = string.Empty;
     [Nombre("Razón Social")]
-    public string Razon_Social { get; private set; } = string.Empty;
-    public string Giro { get; private set; } = string.Empty;
+    public string Razon_Social { get; set; } = string.Empty;
+    public string Giro { get; set; } = string.Empty;
     [Nombre("Dirección")]
-    public string Direccion { get; private set; } = string.Empty;
-    public string Ciudad { get; private set; } = string.Empty;
+    public string Direccion { get; set; } = string.Empty;
+    public string Ciudad { get; set; } = string.Empty;
     [Nombre("Teléfono")]
-    public string Telefono { get; private set; } = string.Empty;
-    public string Fax { get; private set; } = string.Empty;
+    public string Telefono { get; set; } = string.Empty;
+    public string Fax { get; set; } = string.Empty;
     [Nombre("Observación")]
-    public string Observaciones1 { get; private set; } = string.Empty;
-    public int debi { get; private set; }
+    public string Observaciones1 { get; set; } = string.Empty;
+    public int debi { get; set; }
     [Nombre("Debe Inicial")]
-    public int habi { get; private set; }
+    public int habi { get; set; }
     [Nombre("Haber Inicial")]
-    public long debe { get; private set; }
+    public long debe { get; set; }
     [Nombre("Haber")]
-    public long habe { get; private set; }
+    public long habe { get; set; }
     [Nombre("Saldo")]
-    public long saldo { get; private set; }
+    public long saldo { get; set; }
     [Visible(false)]
     public int Empresa { get; set; }
-    public Cliente() { }
-    public Cliente(string rut,
-        string razonSocial,
-        string giro,
-        string direccion,
-        string ciudad,
-        string telefono,
-        string fax,
-        string obs1,
-        int debi,
-        int habi,
-        long debe,
-        long habe,
-        long saldo)
-    {
-        this.Rut = rut;
-        this.Razon_Social = razonSocial;
-        this.Giro = giro;
-        this.Direccion = direccion;
-        this.Ciudad = ciudad;
-        this.Telefono = telefono;
-        this.Fax = fax;
-        this.Observaciones1 = obs1;
-        this.debi = debi;
-        this.habi = habi;
-        this.debe = debe;
-        this.habe = habe;
-        this.saldo = saldo;
-    }
 }
