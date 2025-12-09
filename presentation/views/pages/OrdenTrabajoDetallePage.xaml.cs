d1e5abab686ddf9a84541260fcba84d13a4f134c
Roony11-1 <eltheasasin@gmail.com>
2025-12-09 17:41:59 -0300

    La ventana guarda con la empresa wuwuwuwwuwu

--- presentation/views/pages/OrdenTrabajoDetallePage.xaml.cs
+++ presentation/views/pages/OrdenTrabajoDetallePage.xaml.cs
@@ -1,15 +1,15 @@
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Media;
 using System.Windows.Input;
-using Gestion.core.model;
-
+using Gestion.core.session;
 namespace Gestion.presentation.views.pages;
 
 public partial class OrdenTrabajoDetallePage : Window
 {
     private readonly object _entidadOriginal;
+
     public object EntidadEditada { get; private set; }
+
     public OrdenTrabajoDetallePage(Page padre, object entidad)
     {
         InitializeComponent();
@@ -17,6 +17,7 @@ public partial class OrdenTrabajoDetallePage : Window
 
         _entidadOriginal = entidad;
 
+        // Crear copia profunda simple
         EntidadEditada = Activator.CreateInstance(entidad.GetType())!;
         foreach (var prop in entidad.GetType().GetProperties())
         {
@@ -24,80 +25,26 @@ public partial class OrdenTrabajoDetallePage : Window
                 prop.SetValue(EntidadEditada, prop.GetValue(entidad));
         }
 
+        // Esc para cerrar
         this.PreviewKeyDown += (s, e) =>
         {
             if (e.Key == Key.Escape)
-            {
                 this.DialogResult = false;
-            }
         };
 
-        DataContext = entidad;
-    }
-
-    private static IEnumerable<T> FindControls<T>(DependencyObject parent) where T : DependencyObject
-    {
-        if (parent == null)
-            yield break;
-
-        for (int i = 0; i < VisualTreeHelper.GetChildrenCount(parent); i++)
-        {
-            var child = VisualTreeHelper.GetChild(parent, i);
-
-            if (child is T typedChild)
-                yield return typedChild;
-
-            foreach (var descendant in FindControls<T>(child))
-                yield return descendant;
-        }
+        DataContext = EntidadEditada;
     }
 
     private void BtnGuardar_Click(object sender, RoutedEventArgs e)
     {
-        var textBoxes = FindControls<TextBox>(MainGrid);
-
-        foreach (var prop in EntidadEditada.GetType().GetProperties())
-        {
-            if (!prop.CanWrite)
-                continue;
-
-            // Buscar un TextBox con Tag que coincida con el nombre de la propiedad
-            var txt = textBoxes.FirstOrDefault(t => t.Tag is string tag && tag == prop.Name);
-            
-            if (txt != null)
-            {
-                string texto = txt.Text;
-
-                try
-                {
-                    object valorConvertido = texto;
-
-                    if (prop.PropertyType != typeof(string))
-                        valorConvertido = Convert.ChangeType(texto, prop.PropertyType);
-
-                    prop.SetValue(EntidadEditada, valorConvertido);
-                }
-                catch
-                {
-                    MessageBox.Show($"El valor ingresado para {prop.Name} no es válido.");
-                    txt.Focus();
-                    return;
-                }
-            }
-            else
-            {
-                // Si no hay TextBox para esta propiedad, conservar el valor original
-                var valorOriginal = _entidadOriginal.GetType().GetProperty(prop.Name)?.GetValue(_entidadOriginal);
-                prop.SetValue(EntidadEditada, valorOriginal);
-            }
-        }
-
-        // Forzar el valor de Empresa al final
-        EntidadEditada.GetType().GetProperty("Empresa")?
-            .SetValue(EntidadEditada, _entidadOriginal is OrdenTrabajo ot ? ot.Empresa : 0);
+        EntidadEditada.GetType()
+            .GetProperty("Empresa")?
+            .SetValue(EntidadEditada, SesionApp.IdEmpresa);
 
         this.DialogResult = true;
     }
+
+    
 
     private void BtnCancelar_Click(object sender, RoutedEventArgs e)
     {
