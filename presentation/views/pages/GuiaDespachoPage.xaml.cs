4bbbc2530db858d98a8bf00847320e2f390ffcb2
Roony11-1 <eltheasasin@gmail.com>
2025-12-11 21:00:21 -0300

    Guia Despacho paginacion

--- presentation/views/pages/GuiaDespachoPage.xaml.cs
+++ presentation/views/pages/GuiaDespachoPage.xaml.cs
@@ -23,7 +23,26 @@ public partial class GuiaDespachoPage : Page
 
         Loaded += async (_, _) =>
         {
-            await _viewModel.LoadAllByEmpresa();
+            await _viewModel.LoadPageByEmpresa(1);
+            paginacion.SetTotalPages(_viewModel.TotalRegistros);
+        };
+
+        paginacion.PageChanged += async (nuevaPagina) =>
+        {
+            await _viewModel.LoadPageByEmpresa(nuevaPagina);
+            paginacion.SetTotalPages(_viewModel.TotalRegistros);
+        };
+
+        paginacion.PageSizeChanged += async (size) =>
+        {
+            _viewModel.PageSize = size;
+
+            if (size == 0)
+                await _viewModel.LoadAllByEmpresa(); // sin paginar
+            else
+                await _viewModel.LoadPageByEmpresa(1); // resetear a página 1
+
+            paginacion.SetTotalPages(_viewModel.TotalRegistros);
         };
 
         _dataGrid = dgGuiasDespacho;
